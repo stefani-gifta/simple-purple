d1ea2b33b14cb123c495e7c93b61f403b0d59d26
gifta <152134013+stefani-gifta@users.noreply.github.com>
2025-06-26 15:42:11 +0000

    Separated JS

---
+++ script.js
@@ -0,0 +1,67 @@
+const $bttrlyArray = [];
+var bttrlyInterval = null;
+
+$(document).ready(function () {
+  $('#bttrly-animation').hide();
+  startBttrlyAnimation();
+});
+function startBttrlyAnimation() {
+  bttrlyInterval = setInterval(function () {
+    const $newBttrly = $('#bttrly-animation').clone().removeAttr('id').fadeIn();
+    const randX = Math.floor(Math.random() * ($(window).width() - 100));
+    const randY = Math.floor(Math.random() * ($(window).height() - 100));
+    $newBttrly.css({
+      position: 'absolute',
+      top: randY + 'px',
+      left: randX + 'px',
+      transform: Math.random() <= 0.5 ? 'scaleX(-1)' : 'scaleX(1)'
+    });
+    setInterval(function () {
+      $newBttrly.find('img').toggle();
+    }, 300);
+    $newBttrly.hover(function () {
+      $(this).fadeToggle();
+    });
+    $('body').append($newBttrly);
+    $bttrlyArray.push($newBttrly);
+    $('#remove-bttrly-btn').show();
+    $('#stop-bttrly-btn').show();
+    $('#start-bttrly-btn').hide();
+    $('#start-bttrly-btn').text('let them breed!');
+  }, 3000);
+}
+function toggleRemoveBtn() {
+  $bttrlyArray.forEach($el => $el.remove());
+  $bttrlyArray.length = 0;
+  $('#remove-bttrly-btn').hide();
+}
+function stopBttrlyAnimation() {
+  clearInterval(bttrlyInterval);
+  $('#stop-bttrly-btn').hide();
+  $('#start-bttrly-btn').show();
+}
+$('#start-bttrly-btn').on('click', function () {
+  const $btn = $(this);
+  $btn.text('wait ...');
+  startBttrlyAnimation();
+});
+
+
+
+const buttons = ['Home', 'About', 'Projects', 'Experiments'];
+var active = 'Home';
+
+buttons.forEach((name) => {
+  $(`#btn${name}`).click(function () {
+    $(`#${active.toLowerCase()}`).hide();
+    $(`#btn${active}`)
+      .removeClass('bg-purple-300 translate-x-4 shadow-xl')
+      .addClass('bg-purple-200');
+
+    active = name;
+    $(`#${active.toLowerCase()}`).show();
+    $(`#btn${active}`)
+      .removeClass('bg-purple-200')
+      .addClass('bg-purple-300 translate-x-4 shadow-xl');
+  });
+});
